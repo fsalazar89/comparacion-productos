e471f7b226471051f3b23f71d8de71b260f038ed
fsalazar <fsalazar@flamingo.com.co>
2026-03-07 16:41:54 -0500

    feat - version estable

--- src/config/https.config.ts
+++ src/config/https.config.ts
@@ -10,11 +10,11 @@ export const getHttpsCredentials = (): HttpsCredentials => {
     const sslPath = process.env.CERTIFICATES_SSL_PATH;
 
     if (!sslPath) {
-        throw new Error('CERTIFICATES_SSL_PATH no está definido');
+        throw new Error('CERTIFICATES_SSL_PATH no esta definido');
     }
 
     return {
-        key: fs.readFileSync(path.join(sslPath, 'flamingo.key'), 'utf8'),
-        cert: fs.readFileSync(path.join(sslPath, 'flamingo.crt'), 'utf8'),
+        key: fs.readFileSync(path.join(sslPath, 'xxxxxxxxxx.key'), 'utf8'),
+        cert: fs.readFileSync(path.join(sslPath, 'xxxxxxxxxx.crt'), 'utf8'),
     };
 };
